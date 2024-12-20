605459bba6c527edf4856efcf4bc9b5572ee6917
VonRiddarn <vonriddarn@gmail.com>
2024-12-20 04:23:59 +0100

    docs: added todo comments

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,14 @@ import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+
+// TODO: Indicators
+/*
+	When settings are updated, update the indicator objects.
+	When we want to render, we use the cached indicator objects rather than
+	sending a new query to the settings.
+*/
+
 export function activate(context: vscode.ExtensionContext) {
 	console.log("Character limit indicators extension is now running!");
 }
